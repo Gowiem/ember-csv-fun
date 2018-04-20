61363d7d7704aa4014e771872756aeaeb5914959
Matt Gowie <gowie.matt@gmail.com>
2018-04-20 10:07:42 -0400

    Adds first cut at displaying data-table

---
+++ app/components/data-row.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  tagName: 'tr',
+  columns: computed('data', function() {
+    return this.get('data').split(',');
+  }),
+  headColumn: computed('columns', function() {
+    return this.get('columns.firstObject') || 'Missing columns.';
+  }),
+
+  // TODO: This could be abstracted to an Ember.computed macro for use with DataTable#dataRows
+  // There is also likely a slice macro in Ember Awesome Macros.
+  restColumns: computed('columns', function() {
+    const columns = this.get('columns');
+    // Slice off our header column
+    return columns.slice(1, columns.get('length'));
+  })
+});
--- app/components/data-table.js
+++ app/components/data-table.js
@@ -1,4 +1,17 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  classNames: ['data-table'],
+  allRows: computed('data', function() {
+    return this.get('data').split('\n');
+  }),
+  headers: computed('allRows', function() {
+    return this.get('allRows.firstObject').split(',');
+  }),
+  dataRows: computed('allRows', function() {
+    const allRows = this.get('allRows');
+    // Slice off our header row
+    return allRows.slice(1, allRows.get('length'));
+  })
 });
